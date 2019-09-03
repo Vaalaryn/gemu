48faddf8d61d1a5b51b785f12d66d554a3c9423f
Vaala <brice.bitot@neuf.fr>
2019-09-04 00:08:31 +0200

    Navigation menu

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import {StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
 import {styles} from "./style/globalStyle"
-import NavMenu  from "./Components/navMenu"
+import NavMenu from "./Components/navMenu"
 import ListeView from "./Components/ListeView";
+import {stylesNavMenu} from "./style/navMenuStyle";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.view}>
-        <ListeView/>
-      </View>
-      <NavMenu style={styles.navbar}/>
-    </View>
-  );
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {content: <ListeView/>}
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.view}>
+                    {this.state.content}
+                </View>
+                <View style={stylesNavMenu.navBar}>
+                    <TouchableOpacity
+                        onPress={this.navigation}
+                        style={stylesNavMenu.button}>
+                        <Text style={stylesNavMenu.buttonText}>Liste</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this.navigation}
+                        style={stylesNavMenu.button}>
+                        <Text style={stylesNavMenu.buttonText}>Search</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this.navigation}
+                        style={stylesNavMenu.button}>
+                        <Text style={stylesNavMenu.buttonText}>Profil</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
